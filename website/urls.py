b66ba6cec1e9a15dfba2432d6030cb34eb41f220
Kirsy <hosuthaophuoc@gmail.com>
2024-07-14 21:39:47 +1000

    register

---
+++ website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path('', views.home,name='home'),
+    path('admin/', admin.site.urls),
+    # path('login/', views.login_user,name='login'),
+    path('logout/', views.logout_user,name='logout'),
+    path('register/', views.register_user,name='register'),
+
+]
